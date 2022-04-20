d21050cca1a8c522cee429f44d35d8ac1b26d023
Nesmy Parice BAKALA <nesmypariceb@outlook.com>
2022-04-21 02:06:19 +0800

    Cart without Log-in and with Session Key

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from store.models import Product
+from django.shortcuts import redirect, render
+from carts.models import Cart, CartItem
 
 # Create your views here.
+
+
+def cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)  # get the product
+    try:
+        # get the cart using the cart_id present in the session(cookies)
+        cart = Cart.objects.get(cart_id=cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=cart_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart,
+        )
+    cart_item.save()
+    return HttpResponse(cart_item.quantity)
+    exit()
+    return redirect('cart')
 
 
 def cart(request):
